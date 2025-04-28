62e1129cf0a009c313da9231e42d952c1e97da3c
oldhacker786 <oldhacker7866@gmail.com>
2025-04-28 15:33:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyAz7Cc22lLRcLSR2XSF7lMot_91WxlXfYw"; // Your API key here
+const API_KEY = "AIzaSyCOSwuiMmKKBYPAUnHmve_wu8UfXIoRx6o"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
